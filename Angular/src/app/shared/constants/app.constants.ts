9784837bff7adb7a6808f53997d59bb1a292c5ab
David Nevistic <david.nevistic@calenso.com>
2023-02-10 13:45:13 +0100

    code

--- Angular/src/app/shared/constants/app.constants.ts
+++ Angular/src/app/shared/constants/app.constants.ts
@@ -1,4 +1,5 @@
 export const PARTNER_SETTINGS = {
+  // Hier wurden die Id's der Einstellungen als Konstante gespeichert
   IS_LEXOFFICE_ENABLED: 295,
   LEXOFFICE_API_KEY: 296,
 };
--- CakePHP/src/Helper/Constants.php
+++ CakePHP/src/Helper/Constants.php
@@ -3,6 +3,7 @@ namespace App\Helper;
 
 class Constants
 {
+    // Hier wurden die Id's der Einstellungen als Konstante gespeichert
     const IS_LEX_OFFICE_ENABLED = 295;
     const LEX_OFFICE_API_KEY = 296;
 }
